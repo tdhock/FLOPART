ede3f4893532b00ca9c053329c8762cc8c81cf48
Alyssa Stenberg <alyssajstenberg99@gmail.com>
2021-04-22 10:02:10 -0700

    added tests for correct infinite costs

--- tests/testthat/test-FLOPART.R
+++ tests/testthat/test-FLOPART.R
@@ -1,25 +1,71 @@
-test_that("all up are costs infinite for no peaks",
-          {
-            data_to_test <- 1:10
-            weight_vec <- 1:10
-            data_count <- 10
-            penalty <- 10
-            cost_mat <- 1:10
-            end_vec <- 1:10
-            mean_vec <- 1:10
-            intervals_mat <- 1:10
-            label_starts <- as.integer(3)
-            label_ends <- as.integer(4)
-            label_types <- as.integer(0)
-            label_count <- 3
-          
-            .C("PeakSegFPOPLog_interface", data_to_test, weight_vec, 
-               as.integer(data_count), penalty, cost_mat=cost_mat,
-               end_vec, mean_vec, intervals_mat, label_starts, label_ends, 
-               label_types, label_count)
-            
-            costMatrix <- matrix(cost_mat, data_count, 2)
+library(FLOPART)
+data_count = 5L
+data_vec <- as.integer(c(1,2,3,4,5))
+weight_vec <- as.double(rep(1, data_count))
+penalty <- as.double(0)
+cost_mat <- numeric(data_count * 2)
+end_vec <- integer(data_count)
+mean_vec <- numeric(data_count) 
+intervals_mat <- integer(data_count*2)
+label_starts <- 1L
+label_ends <- 3L
+label_count <- 1L
 
+PoissonLoss <- function(meanVal, dataVal){
+  meanVal - dataVal * log(meanVal)
+  
+}
+
+
+test_that("costs are correct for noPeaks label",
+          {
+            label_types <- 0L
+              .C("FLOPART_interface", data_vec = data_vec, weight_vec = weight_vec,
+                 data_count = data_count, penalty = penalty, cost_mat = cost_mat,
+                 end_vec = end_vec, mean_vec = mean_vec, intervals_mat = intervals_mat,
+                 label_starts = label_starts, label_ends = label_ends,
+                 label_types = label_types, label_count = label_count,
+                 PACKAGE="FLOPART")
             
-          
+            costMatrix <- matrix(result[["cost_mat"]], nrow=2, ncol=data_count, 
+                                 byrow=TRUE)
+            
+            expect_true(is.infinite(costMatrix[1,2]))
+            expect_true(is.infinite(costMatrix[1,3]))
+            expect_true(is.infinite(costMatrix[1,4]))
           })
+test_that("first of peak start cost and last of peak start down costs are infinite",
+          {
+            label_types <- 1L
+            result <- 
+              .C("FLOPART_interface", data_vec = data_vec, weight_vec = weight_vec,
+                 data_count = data_count, penalty = penalty, cost_mat = cost_mat,
+                 end_vec = end_vec, mean_vec = mean_vec, intervals_mat = intervals_mat,
+                 label_starts = label_starts, label_ends = label_ends,
+                 label_types = label_types, label_count = label_count,
+                 PACKAGE="FLOPART")
+            
+            costMatrix <- matrix(result[["cost_mat"]], nrow=2, ncol=data_count, 
+                                 byrow=TRUE)
+            
+            expect_true(is.infinite(costMatrix[1,2]))
+            expect_true(is.infinite(costMatrix[2,4]))
+          })
+test_that("last of peak end down cost and first of peak end up cost are infinite",
+          {
+            label_types <- -1L
+            result <- 
+              .C("FLOPART_interface", data_vec = data_vec, weight_vec = weight_vec,
+                 data_count = data_count, penalty = penalty, cost_mat = cost_mat,
+                 end_vec = end_vec, mean_vec = mean_vec, intervals_mat = intervals_mat,
+                 label_starts = label_starts, label_ends = label_ends,
+                 label_types = label_types, label_count = label_count,
+                 PACKAGE="FLOPART")
+            
+            costMatrix <- matrix(result[["cost_mat"]], nrow=2, ncol=data_count, 
+                                 byrow=TRUE)
+            
+            expect_true(is.infinite(costMatrix[2,2]))
+            expect_true(is.infinite(costMatrix[1,4]))
+          })
+
